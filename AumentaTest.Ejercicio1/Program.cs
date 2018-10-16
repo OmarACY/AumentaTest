0c2fd3fc86b508bec48c6ec27e7dd0c184bd580c
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2018-10-15 20:26:31 -0500

    Implementación de la logica principal

--- AumentaTest.Ejercicio1/Program.cs
+++ AumentaTest.Ejercicio1/Program.cs
@@ -8,14 +8,24 @@ namespace AumentaTest.Ejercicio1
 {
     class Program
     {
+        private static readonly int[] Items = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Introduzca el valor a validar: ");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var inputVal = Console.ReadLine();
+
+            if (int.TryParse(inputVal, out var number))
+            {
+                Console.WriteLine(Items.Contains(number) ? "SI ESTA" : "NO ESTA");
+            }
+            else
+            {
+                Console.WriteLine("El valor introducido debe de ser un entero.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
