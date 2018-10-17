084febce8e48a4bc82afd6d585e2c52915554a20
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2018-10-17 00:45:54 -0500

    Adecuacion del Modelo User y Role

--- AumentaTest.Ejercicio3/Models/User.cs
+++ AumentaTest.Ejercicio3/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -12,5 +13,7 @@ namespace AumentaTest.Ejercicio3.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public ObservableCollection<Role> Roles;
     }
 }
