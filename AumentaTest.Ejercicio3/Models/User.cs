43012e0f518c2e9527fecd7570d2fdcb13b35819
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2018-10-17 00:22:33 -0500

    Se cambio el modelo Usuario por User

--- AumentaTest.Ejercicio3/Models/User.cs
+++ AumentaTest.Ejercicio3/Models/User.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
 namespace AumentaTest.Ejercicio3.Models
 {
-    public class Usuario
+    public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
